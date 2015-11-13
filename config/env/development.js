2e5e46eb0152f6a55dd907a3cac1604e4e8c8b84
Justin <justin_ellery@hotmail.ca>
2015-11-12 19:47:55 -0500

    Figuring out backend file structure.

--- config/env/development.js
+++ config/env/development.js
@@ -2,15 +2,5 @@ var port = 1337;
 
 module.exports = {
     port: port,
-    db: 'mongodb://localhost/users',
-    facebook: {
-        clientID: '1071607026191169',
-        clientSecret: 'bc9723cb9a4063a0b95591295b32508e',
-        callbackURL: 'http://localhost:' + port + '/oauth/facebook/callback'
-    },
-    twitter: {
-        clientID: 'yFntGKkvMZkDKL47XGtzLNdRA',
-        clientSecret: 'EAiPTjPYLX5nrkpRtxYQflbWpRTqqLwwBHRLh7WpdQ1P69Tre6',
-        callbackURL: 'http://localhost:' + port + '/oauth/twitter/callback'
-    }
+    db: 'mongodb://localhost/users'
 };
