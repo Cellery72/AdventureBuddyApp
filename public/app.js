91b2f020905d95e6b1bd32bb1d4e469c8239436e
Justin <justin_ellery@hotmail.ca>
2015-11-16 19:18:43 -0500

    Front-end fun stuff

--- public/app.js
+++ public/app.js
@@ -7,15 +7,28 @@ myApp.config(function ($stateProvider, $urlRouterProvider) {
         .state('home', {
             url: '/',
             views: {
+                'nav': {
+                    templateUrl: '/public/nav/nav.html',
+                    controller: "NavController"
+                },
                 'content': {
                     templateUrl: '/public/content/home.html',
-                    controller: "HomeController"
+                    controller: "HomeController",
+                    views: {
+                        'promo': {
+                            templateUrl: '/public/content/promo.html'
+                        }
+                    }
                 }
             }
         })
         .state('signup', {
             url: '/signup',
             views: {
+                'nav': {
+                    templateUrl: '/public/content/nav.html',
+                    controller: "NavController"
+                },
                 'content': {
                     templateUrl: '/public/content/signup.html',
                     controller: "SignUpController"
@@ -24,6 +37,7 @@ myApp.config(function ($stateProvider, $urlRouterProvider) {
 
         });
 });
+
 myApp.controller('HomeController', function ($scope, $log, $uibModal) {
 
     $scope.animationsEnabled = true;
@@ -83,8 +97,65 @@ myApp.controller('HomeController', function ($scope, $log, $uibModal) {
 
     };
 });
+myApp.controller('NavController', function ($scope, $log, $uibModal) {
+
+    $scope.animationsEnabled = true;
 
 
+    $scope.openLoginModal = function () {
+
+
+
+        var modalInstance = $uibModal.open({
+            animation: $scope.animationsEnabled,
+            templateUrl: 'modal/myModal.html',
+            controller: 'ModalInstanceCtrl',
+            size: 'sm'
+        });
+        modalInstance.result.then(function () {
+
+        }, function () {
+            $log.info('Modal dismissed at: ' + new Date());
+        });
+    };
+    $scope.openRegisterModal = function () {
+
+
+        var modalInstance = $uibModal.open({
+            animation: $scope.animationsEnabled,
+            templateUrl: 'modal/myModal.html',
+            controller: 'ModalInstanceCtrl',
+            resolve: {
+                items: function () {
+                    return $scope.items;
+                }
+            }
+        });
+        modalInstance.result.then(function (selectedItem) {
+
+        }, function () {
+            $log.info('Modal dismissed at: ' + new Date());
+        });
+    };
+
+
+
+    $scope.toggleAnimation = function () {
+        $scope.animationsEnabled = !$scope.animationsEnabled;
+    };
+
+    function showRegisterForm() {
+        $('.loginBox').fadeOut('fast', function () {
+            $('.registerBox').fadeIn('fast');
+            $('.login-footer').fadeOut('fast', function () {
+                $('.register-footer').fadeIn('fast');
+            });
+            $('.modal-title').html('Register with');
+        });
+        $('.error').removeClass('alert alert-danger').html('');
+
+    };
+});
 myApp.controller('ModalInstanceCtrl', ['$uibModalInstance', function ($scope, $uibModalInstace, close) {
 
     $scope.ok = function () {
